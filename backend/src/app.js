c41e907290a365b93129c82e83f38bcde7344011
SudiptaSaha123 <sudiptasaha883@gmail.com>
2025-06-08 14:18:47 +0530

    backend code modified

--- backend/src/app.js
+++ backend/src/app.js
@@ -130,5 +130,5 @@ app.get("/polls/:teacherUsername", (req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(Server running on port ${port}...);
+  console.log(`Server running on port ${port}...`);
 });
